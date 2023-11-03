9f438fe464112a18c26e07a2b75a76d197073c01
moulidharansf4071 <moulidharan.g@syncfusion.com>
2023-11-03 11:27:09 +0530

    Update index.ts

--- ej2-javascript/code-snippet/diagram/getting-started-cs8/index.ts
+++ ej2-javascript/code-snippet/diagram/getting-started-cs8/index.ts
@@ -15,7 +15,7 @@ let nodes: NodeModel[] = [
     {
         id: 'Init', width: 140, height: 50, offsetX: 300, offsetY: 140,
         shape: { type: '', shape: 'Process' },
-        annotations: [{ conFlowtent: 'var i = 0;' }]
+        annotations: [{ content: 'var i = 0;' }]
     }
 ];
 let connectors: ConnectorModel = {
